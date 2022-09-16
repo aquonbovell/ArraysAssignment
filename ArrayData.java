fc5837192add6ab4257953c074b8d7cbc8ea791b
Aquon Bovell <aquon_bovell@outlook.com>
2022-09-16 11:31:45 -0400

    added generate and print functions

--- ArrayData.java
+++ ArrayData.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class ArrayData {
   int rows, columns, values[][], rowData[], colData[];
 
@@ -14,7 +16,10 @@ public class ArrayData {
     initalizeArray(colData);
   }
 
-  ArrayData(int nRows, int nColumns){
+  /*
+   * Augmented Constructor
+   */
+  ArrayData(int nRows, int nColumns) {
     rows = nRows;
     columns = nColumns;
     values = new int[rows][columns];
@@ -25,25 +30,29 @@ public class ArrayData {
     initalizeArray(colData);
   }
   
-  ArrayData(int nRows, int nColumns, int startingValue){
+  /*
+   * Augmented Constructor
+   */
+  ArrayData(int nRows, int nColumns, int startingValue) {
     rows = nRows;
     columns = nColumns;
     values = new int[rows][columns];
     rowData = new int[rows];
     colData = new int[columns];
-    initalizeArray(values , startingValue);
+    initalizeArray(values, startingValue);
     initalizeArray(rowData);
     initalizeArray(colData);
   }
 
-  private void initalizeArray(int array[][]){
+  private void initalizeArray(int array[][]) {
     for (int i = 0; i < array.length; ++i) {
       for (int j = 0; j < array.length; ++j) {
         array[i][j] = 0;
       }
     }
   }
-  private void initalizeArray(int array[][], int startingValue){
+
+  private void initalizeArray(int array[][], int startingValue) {
     for (int i = 0; i < array.length; ++i) {
       for (int j = 0; j < array.length; ++j) {
         array[i][j] = startingValue;
@@ -51,13 +60,31 @@ public class ArrayData {
     }
   }
 
-  private void initalizeArray(int array[]){
+  private void initalizeArray(int array[]) {
     for (int i = 0; i < array.length; ++i) {
-        array[i] = 0;
+      array[i] = 0;
     }
   }
 
-  public void generate(int newValue, int total, int minRow, int maxRow, int minCol, int maxCol){
+  public void generate(int newValue, int total, int minRow, int maxRow, int minCol, int maxCol) {
+    for (int i = 0; i < total; ++i) {
+      values[getRandomNumberInRange(maxRow, minRow)][getRandomNumberInRange(maxCol, minCol)] = newValue;
+    }
+  }
 
+  private int getRandomNumberInRange(int max, int min) {
+    Random rand = new Random();
+    return (rand.nextInt((max - min) +1) + min -1);
+  }
+
+  public void print() {
+    System.out.println("\n-----------------------------------------");
+    for (int i = 0; i < values.length; ++i) {
+      System.out.print("| ");
+      for (int j = 0; j < columns; j++) {
+        System.out.print(values[i][j]+ " | ");
+      }
+      System.out.println("\n-----------------------------------------");
+    }    
   }
 }
--- TestArrayData.java
+++ TestArrayData.java
@@ -1,11 +1,8 @@
-import java.util.Arrays;
-
 public class TestArrayData {
-  
+
   public static void main(String[] args) {
     ArrayData s = new ArrayData();
-    System.out.println(Arrays.deepToString(s.values));
-    System.out.println("\n"+Arrays.toString(s.rowData));
-    System.out.println(Arrays.toString(s.colData));
+    s.generate(6, 10, 2, 4, 2, 5);
+    s.print();
   }
 }
