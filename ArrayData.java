983702f7d2965c1ce2e420f854bb4c83bebcaca5
Aquon Bovell <aquon_bovell@outlook.com>
2022-09-24 20:44:21 -0400

    fixed bugs and errors

--- ArrayData.java
+++ ArrayData.java
@@ -13,10 +13,12 @@ public class ArrayData {
    * Initalizes all elements in rowData, colData, values to 0
    */
   ArrayData() {
-    rows = columns = 10;
-    values = new int[rows][columns];
-    rowData = new int[rows];
-    colData = new int[columns];
+    /* checked */
+    setRows(10);
+    setColumns(10);
+    values = new int[getRows()][getColumns()];
+    rowData = new int[getRows()];
+    colData = new int[getColumns()];
     initalizeArray(values);
     initalizeArray(rowData);
     initalizeArray(colData);
@@ -31,11 +33,12 @@ public class ArrayData {
    * Initalizes all elements in rowData, colData, values to 0
    */
   ArrayData(int nRows, int nColumns) {
-    rows = nRows;
-    columns = nColumns;
-    values = new int[rows][columns];
-    rowData = new int[rows];
-    colData = new int[columns];
+    /* checked */
+    setRows(nRows);
+    setColumns(nColumns);
+    values = new int[getRows()][getColumns()];
+    rowData = new int[getRows()];
+    colData = new int[getColumns()];
     initalizeArray(values);
     initalizeArray(rowData);
     initalizeArray(colData);
@@ -51,24 +54,264 @@ public class ArrayData {
    * Initalizes all values to startingValues
    */
   ArrayData(int nRows, int nColumns, int startingValue) {
-    rows = nRows;
-    columns = nColumns;
-    values = new int[rows][columns];
-    rowData = new int[rows];
-    colData = new int[columns];
+    /* checked */
+    setRows(nRows);
+    setColumns(nColumns);
+    values = new int[getRows()][getColumns()];
+    rowData = new int[getRows()];
+    colData = new int[getColumns()];
     initalizeArrayWithStartingValue(values, startingValue);
     initalizeArray(rowData);
     initalizeArray(colData);
   }
 
+  public void setRows(int rows) {
+    /* checked */
+    if (rows >= 0) {
+      this.rows = rows;
+    } else {
+      this.rows = 0;
+      System.out.println("rows must be positive");
+    }
+  }
+
+  public int getRows() {
+    return rows;
+  }
+
+  public void setColumns(int columns) {
+    /* checked */
+    if (columns >= 0) {
+      this.columns = columns;
+    } else {
+      this.columns = 0;
+      System.out.println("columns must be positive");
+    }
+  }
+
+  public int getColumns() {
+    return columns;
+  }
+
+  public void setColData(int[] colData) {
+    this.colData = colData;
+  }
+
+  public int[] getColData() {
+    return colData;
+  }
+
+  public void setRowData(int[] rowData) {
+    this.rowData = rowData;
+  }
+
+  public int[] getRowData() {
+    return rowData;
+  }
+
+  public void setValues(int[][] values) {
+    this.values = values;
+  }
+
+  public int[][] getValues() {
+    return values;
+  }
+
+  /**
+   * @param newValue
+   * @param total
+   * @param minRow
+   * @param maxRow
+   * @param minCol
+   * @param maxCol
+   */
   public void generate(int newValue, int total, int minRow, int maxRow, int minCol, int maxCol) {
-    for (int i = 0; i < total; ++i) {
-      values[getRandomNumberInRange(maxRow, minRow)][getRandomNumberInRange(maxCol, minCol)] = newValue;
+    /* checked */
+    int replaceableRange = (maxRow - minRow) * (maxCol - minCol);
+    if (replaceableRange >= total) {
+      for (int i = 0; i < total; ++i) {
+        int row = getRandomNumberInRange(maxRow, minRow);
+        int col = getRandomNumberInRange(maxCol, minCol);
+        while (values[row][col] == newValue) {
+          row = getRandomNumberInRange(maxRow, minRow);
+          col = getRandomNumberInRange(maxCol, minCol);
+        }
+        values[row][col] = newValue;
+      }
+    } else {
+      System.out.println("Total must be lower or equal to the replaceable range for the values array.");
+    }
+  }
+
+  public void flip(int num, int val) {
+    /* checked */
+    /* Kenez */
+    Random rand = new Random();
+    int i = 0;
+
+    while (i < num) {
+      int nom = rand.nextInt(rows);
+      int numb = rand.nextInt(columns);
+      values[nom][numb] = val;
+      i++;
+    }
+  }
+
+  public void sum() {
+    /* checked */
+    for (int i = 0; i < rows; ++i) {
+      int sum = 0;
+      for (int j = 0; j < columns; ++j) {
+        sum += values[i][j];
+      }
+      rowData[i] = sum;
+    }
+
+    for (int i = 0; i < rows; ++i) {
+      int sum = 0;
+      for (int j = 0; j < columns; ++j) {
+        sum += values[j][i];
+      }
+      colData[i] = sum;
+    }
+  }
+
+  public void occurrence(int num) {
+    /* checked */
+    for (int i = 0; i < rows; ++i) {
+      int rowOccurrences = 0;
+      for (int j = 0; j < columns; ++j) {
+        if (values[i][j] == num || values[i][j] % num != 0)
+          ++rowOccurrences;
+      }
+      rowData[i] = rowOccurrences;
+    }
+
+    System.out.println("\n\n");
+    for (int i = 0; i < rows; ++i) {
+      int colOccurrences = 0;
+      for (int j = 0; j < columns; ++j) {
+        if (values[j][i] % num == 0)
+        ++colOccurrences;
+      }
+      colData[i] = colOccurrences;
+    }
+  }
+  
+  public void standardDeviation() {
+    /* Dwanye */
+    int sampleSize = 10;
+    double[] y = new double[10];
+    int[] standardDeviaRow = new int[10];
+    double[][] deviationsRow = new double[10][10];
+    double[][] sqDeviationsRow = new double[10][10];
+    double[] sumSqDeviationsRow = new double[10];
+
+    for (int row = 0; row < rowData.length; row++) {
+      for (int col = 0; col < colData.length; col++) {
+        deviationsRow[row][col] = 0;
+        sqDeviationsRow[row][col] = 0;
+      }
+    }
+
+    for (int row = 0; row < rowData.length; row++) {
+      for (int col = 0; col < colData.length; col++) {
+        deviationsRow[row][col] = values[row][col] - averageRow(col);
+      }
+    }
+
+    for (int row = 0; row < rowData.length; row++) {
+      for (int col = 0; col < colData.length; col++) {
+        sqDeviationsRow[row][col] = deviationsRow[row][col] * deviationsRow[row][col];
+      }
+    }
+
+    for (int row = 0; row < rowData.length; row++) {
+      for (int col = 0; col < colData.length; col++) {
+        sumSqDeviationsRow[row] += sqDeviationsRow[row][col];
+      }
+    }
+
+    for (int i = 0; i < rowData.length; i++) {
+      y[i] = sumSqDeviationsRow[i] / sampleSize;
+    }
+
+    for (int i = 0; i < rowData.length; i++) {
+      y[i] = Math.sqrt(y[i]);
+    }
+
+    for (int i = 0; i < rowData.length; i++) {
+      standardDeviaRow[i] = (int) y[i];
+    }
+
+    for (int i = 0; i < rowData.length; i++) {
+      rowData[i] = standardDeviaRow[i];
+    }
+
+    // Doing the same method for columns
+    double[] z = new double[10];
+    int[] standardDeviaCol = new int[10];
+
+    double[][] deviationsCol = new double[10][10];
+    double[][] sqDeviationsCol = new double[10][10];
+    double[] sumSqDeviationsCol = new double[10];
+
+    for (int row = 0; row < rowData.length; row++) {
+      for (int col = 0; col < colData.length; col++) {
+        deviationsCol[row][col] = 0;
+        sqDeviationsCol[row][col] = 0;
+      }
+    }
+
+    for (int row = 0; row < rowData.length; row++) {
+      for (int col = 0; col < colData.length; col++) {
+        deviationsCol[col][row] = values[col][row] - averageCol(col);
+      }
+    }
+
+    for (int row = 0; row < rowData.length; row++) {
+      for (int col = 0; col < colData.length; col++) {
+        sqDeviationsCol[col][row] = deviationsCol[col][row] * deviationsCol[col][row];
+      }
+    }
+
+    for (int row = 0; row < rowData.length; row++) {
+      for (int col = 0; col < colData.length; col++) {
+        sumSqDeviationsCol[row] += sqDeviationsCol[col][row];
+      }
+    }
+
+    for (int i = 0; i < rowData.length; i++) {
+      z[i] = sumSqDeviationsCol[i] / sampleSize;
+    }
+
+    for (int i = 0; i < rowData.length; i++) {
+      z[i] = Math.sqrt(z[i]);
+    }
+
+    for (int i = 0; i < rowData.length; i++) {
+      standardDeviaCol[i] = (int) z[i];
+    }
+
+    for (int i = 0; i < rowData.length; i++) {
+      colData[i] = standardDeviaCol[i];
+    }
+
+    // temp for outputing data to the screen.
+    for (int row = 0; row < rowData.length; row++) {
+      System.out.println(rowData[row]);
+    }
+
+    System.out.println();
+
+    for (int row = 0; row < rowData.length; row++) {
+      System.out.println(colData[row]);
     }
   }
 
   /**
-   * Displays all the elements in the ith row along with the ith element in rowData 
+   * Displays all the elements in the ith row along with the ith element in
+   * rowData
    * separated by two pipes("||") and finally all the elements in the colData
    */
   public void print() {
@@ -124,46 +367,6 @@ public class ArrayData {
     System.out.println();
   }
 
-  public void sum() {
-
-    for (int i = 0; i < rowData.length; ++i) {
-      int sum = 0;
-      for (int index = 0; index < columns; ++index) {
-        sum += values[i][index];
-      }
-      rowData[i] = sum;
-    }
-
-    for (int i = 0; i < colData.length; ++i) {
-      int sum = 0;
-      for (int index = 0; index < columns; ++index) {
-        sum += values[index][i];
-      }
-      colData[i] = sum;
-    }
-  }
-
-  public void occurrence(int num) {
-    for (int i = 0; i < values.length; i++) {
-      int rowOccurrences = 0;
-      for (int j = 0; j < columns; j++) {
-        if (values[i][j] == num || values[i][j] % num != 0)
-          ++rowOccurrences;
-      }
-      System.out.println(rowOccurrences);
-    }
-
-    System.out.println("\n\n");
-    for (int j = 0; j < columns; ++j) {
-      int colOccurrences = 0;
-      for (int i = 0; i < values.length; ++i) {
-        if (values[i][j] != 0 && values[i][j] % num == 0)
-          ++colOccurrences;
-      }
-      System.out.println(colOccurrences);
-    }
-  }
-
   public void product(int min, int max) {
     /* Kenez */
     int prodCol[], prodRow[];
@@ -198,120 +401,6 @@ public class ArrayData {
         colData[j] = prodCol[j];
         j++;
       }
-    }
-  }
-
-  public void flip(int num, int val) {
-    /* Kenez */
-    Random rand = new Random();
-    int i = 0;
-
-    while (i <= num) {
-      int nom = rand.nextInt(rows);
-      int numb = rand.nextInt(columns);
-      values[nom][numb] = val;
-      i++;
-    }
-  }
-
-  public void standardDeviation() {
-    /* Dwanye */
-    int sampleSize = 10;
-    double[] y = new double[10];
-    int[] standardDeviaRow = new int[10];
-    double[][] deviationsRow = new double[10][10];
-    double[][] sqDeviationsRow = new double[10][10];
-    double[] sumSqDeviationsRow = new double[10];
-
-    for (int row = 0; row < rowData.length; row++) {
-      for (int col = 0; col < colData.length; col++) {
-        deviationsRow[row][col] = 0;
-        sqDeviationsRow[row][col] = 0;
-      }
-    }
-
-    for (int row = 0; row < rowData.length; row++) {
-      for (int col = 0; col < colData.length; col++) {
-        deviationsRow[row][col] = values[row][col] - averageRow(col);
-      }
-    }
-
-    for (int row = 0; row < rowData.length; row++) {
-      for (int col = 0; col < colData.length; col++) {
-        sqDeviationsRow[row][col] = deviationsRow[row][col] * deviationsRow[row][col];
-      }
-    }
-    for (int row = 0; row < rowData.length; row++) {
-      for (int col = 0; col < colData.length; col++) {
-        sumSqDeviationsRow[row] += sqDeviationsRow[row][col];
-      }
-    }
-    for (int i = 0; i < rowData.length; i++) {
-      y[i] = sumSqDeviationsRow[i] / sampleSize;
-    }
-    for (int i = 0; i < rowData.length; i++) {
-      y[i] = Math.sqrt(y[i]);
-    }
-    for (int i = 0; i < rowData.length; i++) {
-      standardDeviaRow[i] = (int) y[i];
-    }
-    for (int i = 0; i < rowData.length; i++) {
-      rowData[i] = standardDeviaRow[i];
-    }
-
-    // Doing the same method for columns
-    double[] z = new double[10];
-    int[] standardDeviaCol = new int[10];
-
-    double[][] deviationsCol = new double[10][10];
-    double[][] sqDeviationsCol = new double[10][10];
-    double[] sumSqDeviationsCol = new double[10];
-
-    for (int row = 0; row < rowData.length; row++) {
-      for (int col = 0; col < colData.length; col++) {
-        deviationsCol[row][col] = 0;
-        sqDeviationsCol[row][col] = 0;
-      }
-    }
-
-    for (int row = 0; row < rowData.length; row++) {
-      for (int col = 0; col < colData.length; col++) {
-        deviationsCol[col][row] = values[col][row] - averageCol(col);
-      }
-    }
-
-    for (int row = 0; row < rowData.length; row++) {
-      for (int col = 0; col < colData.length; col++) {
-        sqDeviationsCol[col][row] = deviationsCol[col][row] * deviationsCol[col][row];
-      }
-    }
-    for (int row = 0; row < rowData.length; row++) {
-      for (int col = 0; col < colData.length; col++) {
-        sumSqDeviationsCol[row] += sqDeviationsCol[col][row];
-      }
-    }
-    for (int i = 0; i < rowData.length; i++) {
-      z[i] = sumSqDeviationsCol[i] / sampleSize;
-    }
-    for (int i = 0; i < rowData.length; i++) {
-      z[i] = Math.sqrt(z[i]);
-    }
-    for (int i = 0; i < rowData.length; i++) {
-      standardDeviaCol[i] = (int) z[i];
-    }
-    for (int i = 0; i < rowData.length; i++) {
-      colData[i] = standardDeviaCol[i];
-    }
-
-    // temp for outputing data to the screen.
-    for (int row = 0; row < rowData.length; row++) {
-      System.out.println(rowData[row]);
-
-    }
-    System.out.println();
-    for (int row = 0; row < rowData.length; row++) {
-      System.out.println(colData[row]);
-
     }
   }
 
@@ -362,7 +451,7 @@ public class ArrayData {
 
   private int getRandomNumberInRange(int max, int min) {
     Random rand = new Random();
-    return (rand.nextInt((max - min) + 1) + min - 1);
+    return (rand.nextInt(max - min) + min - 1);
   }
 
   private double averageRow(int r) {
--- TestArrayData.java
+++ TestArrayData.java
@@ -1,15 +1,22 @@
+import java.util.Arrays;
+
 public class TestArrayData {
 
   public static void main(String[] args) {
     ArrayData s = new ArrayData();
-    s.generate(6, 99, 1, 9, 1, 8);
+    s.generate(6, 30, 1, 8, 1, 8);
+
+    s.print();
+
+    s.flip(50, 2);
 
     s.print();
 
     s.sum();
 
-    s.product(5, 9);
+    s.occurrence(6);
 
-    s.occurrence(4);
+    System.out.println(Arrays.toString(s.getColData()));
+    System.out.println(Arrays.toString(s.getRowData()));
   }
 }
